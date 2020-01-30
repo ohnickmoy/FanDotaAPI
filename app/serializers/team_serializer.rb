f33ad9f0f314417275c80fef128d426506d355a7
Nicholas Moy <nicksmoy@gmail.com>
2020-01-30 12:38:36 -0500

    added api_id to serializer, added corresspoding migration for api_id

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,6 +1,6 @@
 class TeamSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :tag, :logo_url, :wins, :losses
+  attributes :name, :tag, :logo_url, :wins, :losses, :api_id
   #line below works if i want detailed data on users, not sure how to fix aliasing issue
   # has_many :users
   has_many :team_followers
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_164254) do
+ActiveRecord::Schema.define(version: 2020_01_29_220111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_31_164254) do
     t.integer "losses"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "api_id"
   end
 
   create_table "users", force: :cascade do |t|
